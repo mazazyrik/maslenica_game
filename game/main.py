3bc9a8da21286f5d6eb875043b15c97f5a3d1917
mazazyrik <117200062+mazazyrik@users.noreply.github.com>
2025-02-18 11:21:49 +0300

    first

---
+++ game/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from routers import api, template_routers
+
+app = FastAPI(title='Maslenica')
+
+app.include_router(api.router)
+app.include_router(template_routers.router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
