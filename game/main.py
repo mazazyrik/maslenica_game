697fdd1a2e3ea154520281bdd738d2f25666cc71
mazazyrik <mazazyrik@yandex.ru>
2025-02-23 16:49:42 +0300

    index full ready

--- game/main.py
+++ game/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from routers import api, template_routers
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(title='Maslenica')
 app.mount('/static', StaticFiles(directory='static'), name='static')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(api.router)
--- game/routers/api.py
+++ game/routers/api.py
@@ -1,9 +1,55 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+from db import User as UserModel
+
+
+class User(BaseModel):
+    username: str
+    score: int
+
+
+class Score(BaseModel):
+    score: int
 
 
 router = APIRouter(prefix='/api')
 
 
 @router.post('/users')
-async def create_user():
-    return
+async def create_user(user: User):
+    if UserModel.select().where(UserModel.username == user.username).exists():
+        raise HTTPException(
+            status_code=400, detail='Пользователь уже существует')
+    UserModel.create(username=user.username, score=user.score)
+    return {'username': user.username, 'score': user.score}
+
+
+@router.get('/users/{username}')
+async def get_user(username: str):
+    if UserModel.select().where(UserModel.username == username).exists():
+        user = UserModel.select().where(UserModel.username == username).get()
+        return {'username': user.username, 'score': user.score}
+    raise HTTPException(status_code=404, detail='Пользователь не найден')
+
+
+@router.put('/users/{username}')
+async def update_user(username: str, score: Score):
+    if UserModel.select().where(UserModel.username == username).exists():
+        UserModel.update(username=username, score=score.score).where(
+            UserModel.username == username).execute()
+        return {'username': username, 'score': score.score}
+    raise HTTPException(status_code=404, detail='Пользователь не найден')
+
+
+@router.delete('/users/{username}')
+async def delete_user(username: str):
+    if UserModel.select().where(UserModel.username == username).exists():
+        UserModel.delete().where(UserModel.username == username).execute()
+        return {'message': 'Пользователь удален'}
+    raise HTTPException(status_code=404, detail='Пользователь не найден')
+
+
+@router.get('/users')
+async def get_users():
+    users = UserModel.select()
+    return [{'username': user.username, 'score': user.score} for user in users]
