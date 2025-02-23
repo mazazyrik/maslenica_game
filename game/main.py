8c9e01eaa38e70b1ebf1f1bffcdf077d44c3bfbd
mazazyrik <117200062+mazazyrik@users.noreply.github.com>
2025-02-23 19:16:41 +0300

    ver 1.1

--- game/main.py
+++ game/main.py
@@ -8,24 +8,25 @@ app = FastAPI(title='Maslenica')
 app.mount('/static', StaticFiles(directory='static'), name='static')
 
 origins = [
-    "http://194.87.216.212",
-    "http://194.87.216.212:8000",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
+    'https://education-marathon.ddns.net'
+    'http://194.87.216.212',
+    'http://194.87.216.212:8000',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
 
 app.include_router(api.router)
 app.include_router(template_routers.router)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host='127.0.0.1', port=8000)
