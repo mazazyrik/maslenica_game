30546d0079961e1859e1c5e754d67323e34012ff
mazazyrik <mazazyrik@yandex.ru>
2025-02-24 13:15:54 +0300

    ver 1.3 fixes

--- game/main.py
+++ game/main.py
@@ -8,11 +8,11 @@ app = FastAPI(title='Maslenica')
 app.mount('/static', StaticFiles(directory='static'), name='static')
 
 origins = [
-    'https://education-marathon.ddns.net'
+    'http://127.0.0.1:8000',
     'http://194.87.216.212',
     'http://194.87.216.212:8000',
     'http://localhost:8000',
-    'https://education-marathon.ddns.net',
+    'http://127.0.0.1:8000',
 ]
 
 app.add_middleware(
